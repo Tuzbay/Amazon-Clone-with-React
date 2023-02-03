675c62de886609b97ae10966cf29fa24c6058daa
Tuzbay <t.uzbayyelce@gmail.com>
2023-02-03 17:43:53 +0300

    Banner Added

--- src/App.js
+++ src/App.js
@@ -1,81 +1,28 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/checkokut" element={<h1>Checkout</h1>} />
+        <Route
+          path="/checkout"
+          element={
+            <>
+              <Header />
+              <h1>Checkout</h1>
+            </>
+          }
+        />
         <Route path="/login" element={<h1>Login</h1>} />
         <Route
           path="/"
           element={
             <>
               <Header />
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
-              <div>HOME PAGE!</div>
+              <Home />
             </>
           }
         />
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <img
+        src="https://m.media-amazon.com/images/I/71N3Vmy5V4L._SX3000_.jpg"
+        alt=""
+        className="home__image"
+      />
+    </div>
+  );
+}
+
+export default Home;
